9fea087affc35e849802397cb37593ad2e324bec
2c2048d2 <dyq233@qq.com>
2024-08-10 16:28:37 +0800

    support python3.11 or lower

--- src/plugins/ACMRaceBot/__init__.py
+++ src/plugins/ACMRaceBot/__init__.py
@@ -2,7 +2,6 @@ from nonebot import get_plugin_config, logger, on_command
 from nonebot.adapters import Message
 from nonebot.adapters.onebot.v11 import MessageSegment
 from nonebot.params import CommandArg
-from nonebot.plugin import PluginMetadata
 
 from .API import (
     fetchAtcoderRaces,
@@ -16,13 +15,6 @@ from .API import (
 from .config import Config
 from .models import RaceInfo
 
-__plugin_meta__ = PluginMetadata(
-    name="hyc_race",
-    description="",
-    usage="",
-    config=Config,
-)
-
 config = get_plugin_config(Config)
 
 logger.debug(config)
@@ -32,12 +24,13 @@ def gen_message(data: list[RaceInfo]) -> str:
     output = ""
     for i in data:
         minutes = i.duration_minutes % 60
-        output += (f"{i.title}\n"
-                   f"开始时间：{i.start_time.to(
-                       'Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')}\n"
-                   f"比赛时长：{int(i.duration_minutes / 60)}小时"
-                   f"{f'{minutes}分钟' if minutes > 0 else ''}\n"
-                   f"传送门->：{i.url}\n\n")
+        output += f"""{i.title}
+开始时间：{i.start_time.to('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss')}
+比赛时长：{int(i.duration_minutes / 60)}小时
+{f'{minutes}分钟' if minutes > 0 else ''}
+传送门->：{i.url}
+
+"""
 
     return output if data else "没有获取到数据哦"
 
